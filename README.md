08802eb9ac1103bc0137fa7381d68b9a4d51089b
AlejandroXVII <110202680+AlejandroXVII@users.noreply.github.com>
2024-02-23 02:40:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+React template
+
+include vest and router
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import { Link } from "react-router-dom";
+
+const App = () => {
+	return (
+		<div>
+			<h1>Hello from the main page of the app!</h1>
+			<p>EXAMPLE TEXT TO SHOW HOW IT SEEM</p>
+			<div>
+				<button className="savedBtn">Accept</button>
+				<button>Cancel</button>
+			</div>
+		</div>
+	);
+};
+
+export default App;
